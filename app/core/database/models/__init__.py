4a8f88155cbae15252d423e1ea1bdd7c1de1eb19
kramber1024 <kramber268@gmail.com>
2024-07-25 13:08:47 +0300

    feat: Add Tag to __all__

--- app/core/database/models/__init__.py
+++ app/core/database/models/__init__.py
@@ -1,11 +1,13 @@
 from .click import Click
 from .status import Status
+from .tag import Tag
 from .url import Url
 from .user import User
 
 __all__ = (
     "Click",
     "Status",
+    "Tag",
     "Url",
     "User",
 )

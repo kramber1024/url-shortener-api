aa7b35080a6ea3e064d175af1e0e4074e1a46a7a
kramber1024 <kramber268@gmail.com>
2024-07-26 14:22:00 +0300

    feat: Add utils for openapi documentation

---
+++ app/api/responses.py
@@ -0,0 +1,52 @@
+from typing import Any
+
+from pydantic import BaseModel
+
+from app.api.schemes import ErrorResponse as ErrorResponseScheme
+
+type Response = dict[str, Any]
+
+
+def response(
+    description: str,
+    model: type[BaseModel],
+    example: dict[str, Any] | None = None,
+) -> Response:
+    response: Response = {
+        "description": description,
+        "model": model,
+    }
+
+    if example is not None:
+        response.update(
+            {
+                "content": {
+                    "application/json": {
+                        "example": example,
+                    },
+                },
+            },
+        )
+
+    return response
+
+
+INVALID_TOKEN: Response = response(
+    description="Provided token is not valid.",
+    model=ErrorResponseScheme,
+    example={
+        "errors": [],
+        "message": "Invalid token",
+        "status": 400,
+    },
+)
+
+INTERNAL_SERVER_ERROR: Response = response(
+    description="Something went very wrong. Please report this issue.",
+    model=ErrorResponseScheme,
+    example={
+        "errors": [],
+        "message": "Internal server error",
+        "status": 500,
+    },
+)

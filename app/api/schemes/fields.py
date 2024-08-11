8853d83c122dea5a15b62ba28284cc5f9f111f5a
kramber1024 <kramber268@gmail.com>
2024-08-11 10:16:29 +0300

    refactor: Move __all__ to the top

--- app/api/schemes/fields.py
+++ app/api/schemes/fields.py
@@ -1,66 +1,6 @@
-from typing import Annotated
+from typing import Annotated, TypeAlias
 
 from pydantic import EmailStr, Field
-
-HTTPStatus = Annotated[
-    int,
-    Field(
-        description="HTTP status code.",
-        examples=[422],
-        ge=100,
-        le=599,
-    ),
-]
-
-Id = Annotated[
-    str,
-    Field(
-        description="Unique identifier.",
-        examples=["7205626878688008192"],
-        max_length=len("7205626878688008192"),
-        min_length=len("7205626878688008192"),
-    ),
-]
-
-Email = Annotated[
-    EmailStr,
-    Field(
-        min_length=len("*@*.*"),
-        max_length=64,
-        description="Email used for authentication and notifications.",
-        examples=["email@domain.tld"],
-    ),
-]
-
-Password = Annotated[
-    str,
-    Field(
-        min_length=8,
-        max_length=256,
-        description="Used for authentication.",
-        examples=["My$uper$ecretPa$$word"],
-    ),
-]
-
-FirstName = Annotated[
-    str,
-    Field(
-        min_length=3,
-        max_length=16,
-        description="Used in full name and official emails",
-        examples=["John"],
-    ),
-]
-
-LastName = Annotated[
-    str,
-    Field(
-        min_length=3,
-        max_length=16,
-        description="Used in full name.",
-        examples=["Doe"],
-    ),
-]
 
 __all__ = (
     "Email",
@@ -70,3 +10,64 @@ __all__ = (
     "LastName",
     "Password",
 )
+
+Email: TypeAlias = Annotated[
+    EmailStr,
+    Field(
+        min_length=len("*@*.*"),
+        max_length=64,
+        description="Email used for authentication and notifications.",
+        examples=["email@domain.tld"],
+    ),
+]
+
+FirstName: TypeAlias = Annotated[
+    str,
+    Field(
+        min_length=3,
+        max_length=16,
+        description="Used in full name and official emails",
+        examples=["John"],
+    ),
+]
+
+HTTPStatus: TypeAlias = Annotated[
+    int,
+    Field(
+        description="HTTP status code.",
+        examples=[422],
+        ge=100,
+        le=599,
+    ),
+]
+
+Id: TypeAlias = Annotated[
+    str,
+    Field(
+        description="Unique identifier.",
+        examples=["7205626878688008192"],
+        max_length=len("7205626878688008192"),
+        min_length=len("7205626878688008192"),
+    ),
+]
+
+
+LastName: TypeAlias = Annotated[
+    str,
+    Field(
+        min_length=3,
+        max_length=16,
+        description="Used in full name.",
+        examples=["Doe"],
+    ),
+]
+
+Password: TypeAlias = Annotated[
+    str,
+    Field(
+        min_length=8,
+        max_length=256,
+        description="Used for authentication.",
+        examples=["My$uper$ecretPa$$word"],
+    ),
+]

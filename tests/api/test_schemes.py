90eb112a8c6cb769dc69a175f734525d190753f3
kramber1024 <kramber268@gmail.com>
2024-08-12 10:08:41 +0300

    feat: Refactor test to use user_credentials  fixture

--- tests/api/test_schemes.py
+++ tests/api/test_schemes.py
@@ -2,22 +2,11 @@ from app.api import schemes
 from app.core.database import models
 
 
-def test_user_scheme() -> None:
-    first_name: str = "Dillon"
-    last_name: str = "Carroll"
-    email: str = "Dovie87@gmail.com"
-    password: str = "YzbXhViCSId_pJK"
-
-    user_model: models.User = models.User(
-        first_name=first_name,
-        last_name=last_name,
-        email=email,
-        password=password,
-    )
-    user_scheme: schemes.User = schemes.User.from_model(user_model)
+def test_user_scheme(user_credentials: models.User) -> None:
+    user_scheme: schemes.User = schemes.User.from_model(user_credentials)
 
     assert isinstance(user_scheme, schemes.User)
-    assert user_scheme.id == str(user_model.id)
-    assert user_scheme.first_name == user_model.first_name
-    assert user_scheme.last_name == user_model.last_name
-    assert user_scheme.email == user_model.email
+    assert user_scheme.id == str(user_credentials.id)
+    assert user_scheme.first_name == user_credentials.first_name
+    assert user_scheme.last_name == user_credentials.last_name
+    assert user_scheme.email == user_credentials.email

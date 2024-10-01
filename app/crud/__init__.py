6be6931b4894bd1255a3f5eb2342f8bcafe61765
kramber <kramber268@gmail.com>
2024-10-01 10:50:45 +0300

    feat: Update namespace with new functions

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,9 +1,11 @@
+from .click import create_click
 from .status import create_status
 from .tag import create_tag
-from .url import create_url, get_url_by_slug
+from .url import create_url, get_url_by_slug, update_url
 from .user import create_user, get_user_by_email, get_user_by_id
 
 __all__ = (
+    "create_click",
     "create_status",
     "create_tag",
     "create_url",
@@ -11,4 +13,5 @@ __all__ = (
     "get_url_by_slug",
     "get_user_by_email",
     "get_user_by_id",
+    "update_url",
 )

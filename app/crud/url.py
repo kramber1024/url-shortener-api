c2fcbf464009c2095b80f9b78a5b174f1735dae6
kramber <kramber268@gmail.com>
2024-10-01 09:54:32 +0300

    feat: Add function for updating Url attributes

--- app/crud/url.py
+++ app/crud/url.py
@@ -39,3 +39,33 @@ async def get_url_by_slug(
     url: Url | None = result.scalars().first()
 
     return url
+
+
+async def update_url(
+    *,
+    session: AsyncSession,
+    url: Url,
+    slug: str | None = None,
+    address: str | None = None,
+    total_clicks: int | None = None,
+) -> Url:
+    """Update the ` Url ` attributes with the given values.
+
+    Args:
+        session (AsyncSession): The database session.
+        url (Url): The ` Url ` instance to update.
+        slug (str | None, optional): New slug.
+        address (str | None, optional): New address.
+        total_clicks (int | None, optional): New total_clicks value.
+
+    Returns:
+        Url: The updated ` Url ` instance.
+    """
+    url.slug = slug or url.slug
+    url.address = address or url.address
+    url.total_clicks = total_clicks or url.total_clicks
+
+    session.add(url)
+    await session.commit()
+    await session.refresh(url)
+    return url

2b0d1e83bc52871ce275893a4efb64425a741b25
kramber <kramber268@gmail.com>
2024-10-03 15:28:54 +0300

    feat: Enable logging

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,4 +1,9 @@
 import logging
-from logging import Logger
+import sys
+from logging import Logger, StreamHandler
+from typing import TextIO
 
 logger: Logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler: StreamHandler[TextIO] = logging.StreamHandler(sys.stdout)
+logger.addHandler(handler)

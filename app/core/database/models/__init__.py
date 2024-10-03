a0e5d3a5550edf7e5795c22886ccc08202b79700
kramber <kramber268@gmail.com>
2024-10-03 14:05:22 +0300

    chore: Add network to all

--- app/core/database/models/__init__.py
+++ app/core/database/models/__init__.py
@@ -1,8 +1,9 @@
 from .click import Click
 from .mixins import Base
+from .network import Network
 from .status import Status
 from .tag import Tag
 from .url import Url
 from .user import User
 
-__all__ = ("Base", "Click", "Status", "Tag", "Url", "User")
+__all__ = ("Base", "Click", "Network", "Status", "Tag", "Url", "User")

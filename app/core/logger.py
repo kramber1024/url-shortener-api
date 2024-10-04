9a8edc382ad2cbd3186034309a5f46acff4a8627
kramber <kramber268@gmail.com>
2024-10-04 10:25:13 +0300

    feat: Update logger

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,9 +1,13 @@
 import logging
 import sys
-from logging import Logger, StreamHandler
+from logging import Formatter, Logger, StreamHandler
 from typing import TextIO
 
 logger: Logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
 handler: StreamHandler[TextIO] = logging.StreamHandler(sys.stdout)
 logger.addHandler(handler)
+
+formatter: Formatter = logging.Formatter("%(levelname)s:     %(message)s")
+handler.setFormatter(formatter)

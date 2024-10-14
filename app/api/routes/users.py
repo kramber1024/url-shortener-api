56c0fbdf9d57aea573de26b1ef339a617ca0686b
kramber <kramber268@gmail.com>
2024-10-14 17:16:48 +0300

    feat: Add endpoint for getting urls chore: Update examples

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -21,19 +21,16 @@ router: APIRouter = APIRouter(
     summary="Get current user",
     description="Get information about the current user",
     responses={
-        status.HTTP_200_OK: {
-            "description": "User information retrieved successfully.",
-            "model": schemes.User,
-            "content": {
-                "application/json": {
-                    "example": {
-                        "id": "7207306656936357888",
-                        "name": "kramber",
-                        "email": "email@domain.tld",
-                    },
-                },
+        status.HTTP_200_OK: responses.response(
+            description="User information retrieved successfully.",
+            model=schemes.User,
+            example={
+                "email": "email@domain.tld",
+                "first_name": "John",
+                "last_name": "Doe",
+                "id": "7250096872525709312",
             },
-        },
+        ),
     },
 )
 def get_users_me(
@@ -44,5 +41,18 @@ def get_users_me(
 ) -> JSONResponse:
     return JSONResponse(
         content=schemes.User.from_model(user).model_dump(),
+        status_code=status.HTTP_200_OK,
+    )
+
+
+@router.get("/me/urls", summary="Get all")
+async def get_users_me_urls(
+    user: Annotated[
+        User,
+        Depends(jwt_.get_current_user),
+    ],
+) -> JSONResponse:
+    return JSONResponse(
+        content=schemes.UrlList.from_model(user.urls).model_dump(),
         status_code=status.HTTP_200_OK,
     )

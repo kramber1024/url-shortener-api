c36068d6850762592773ed6bd4c1fd9520e07881
kramber <kramber268@gmail.com>
2024-10-24 12:29:37 +0300

    refactor: Minor changes

--- tests/utils.py
+++ tests/utils.py
@@ -8,7 +8,9 @@ USER_FIRST_NAME: str = "John"
 USER_LAST_NAME: str = "Doe"
 USER_EMAIL: str = "Freddie.Bosco@exAmple.com"
 USER_PHONE: str = "1(234)-567-89-01"
-USER_PASSWORD: str = "QKY80gAKwxebKGbWG2LPOqnSiFLanjomx8cWUzQjWuhLkueiUlhtkScUVZGdJ"
+USER_PASSWORD: str = (
+    "QKY80gAKwxebKGbWG2LPOqnSiFLanjomx8cWUzQjWuhLkueiUlhtkScUVZGdJ"
+)
 
 
 def error_type_exists(json: dict[str, Any], error_type: str) -> bool:
@@ -24,8 +26,23 @@ def format_email(email: str) -> str:
     return f"{email_splitted[0]}@{email_splitted[1].lower()}"
 
 
-def get_token_exp(type_: Literal["access", "refresh"], current_time: int) -> int:
-    if type_ == "access":
+def get_token_exp(
+    jwt_type: Literal["access", "refresh"],
+    /,
+    *,
+    current_time: int,
+) -> int:
+    """Get the expiration time for a JWT token.
+
+    Args:
+        jwt_type (Literal["access", "refresh"]): The type of JWT token. Can be
+                                                 either "access" or "refresh".
+        current_time (int): The current time in seconds since the epoch.
+
+    Returns:
+        int: The expiration time in seconds since the epoch.
+    """
+    if jwt_type == "access":
         return current_time + settings.jwt.ACCESS_TOKEN_EXPIRES_MINUTES * 60
 
     return current_time + settings.jwt.REFRESH_TOKEN_EXPIRES_DAYS * 24 * 60 * 60

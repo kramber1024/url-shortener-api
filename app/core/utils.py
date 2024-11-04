5f0f73882193f18030365b3028f9af23bdf6767e
kramber <kramber268@gmail.com>
2024-11-04 15:48:03 +0300

    refactor: Move base10_to_urlsafe_base64 and make it private

--- app/core/utils.py
+++ app/core/utils.py
@@ -1,4 +1,3 @@
-import base64
 import datetime
 from typing import TYPE_CHECKING
 
@@ -24,37 +23,6 @@ def now() -> int:
 
     """
     return int(datetime.datetime.now(datetime.UTC).timestamp())
-
-
-def base10_to_urlsafe_base64(number: int, /) -> str:
-    """Convert a base 10 number to a URL safe base 64 string.
-
-    Args:
-        number (int): The base 10 number to convert.
-
-    Returns:
-        The URL safe base 64 string.
-
-    Examples:
-    >>> url_safe_base64 = (
-    ...     base10_to_urlsafe_base64(
-    ...         123456789
-    ...     )
-    ... )
-    >>> print(url_safe_base64)
-    "B1vNFQ"
-
-    """
-    byte_representation: bytes = number.to_bytes(
-        (number.bit_length() + 7) // 8,
-        byteorder="big",
-    )
-
-    base64_encoded: bytes = base64.b64encode(byte_representation)
-
-    return (
-        base64_encoded.decode("utf-8").replace("+", "-").replace("/", "_").rstrip("=")
-    )
 
 
 async def get_country_by_ip(*, session: AsyncSession, ip: str) -> str | None:

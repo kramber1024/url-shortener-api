b8191675509dca2a7b6010b9b7a911d8dd9b9916
kramber <kramber268@gmail.com>
2024-11-25 16:14:50 +0300

    feat:  Add address

--- app/core/data/__init__.py
+++ app/core/data/__init__.py
@@ -1,6 +1,7 @@
-from .address import Email
+from .address import Address
 from .first_name import FirstName
 from .last_name import LastName
+from .mail import Email
 from .page import Page
 from .password import Password
 from .phone import Phone
@@ -8,6 +9,7 @@ from .slug import Slug
 from .tag import Tag
 
 __all__ = [
+    "Address",
     "Email",
     "FirstName",
     "LastName",
--- app/core/data/url.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class Url(int, Enum):
-    MIN_LENGTH: int = len("http://a.b")
-    MAX_LENGTH: int = 2048
-    USER_MAX_AMOUNT: int = 2048
-    USER_MAX_TAGS_AMOUNT: int = 32

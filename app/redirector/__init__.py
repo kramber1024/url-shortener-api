bd4525ee6adbacd02b6f6b05d72f3e4d6edc91e5
kramber <kramber268@gmail.com>
2024-11-28 21:07:53 +0300

    feat: Update dependencies

--- app/redirector/__init__.py
+++ app/redirector/__init__.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 
+from .controller import router
+
 redirector: APIRouter = APIRouter(prefix="", include_in_schema=False)
+redirector.include_router(router)
 
 __all__ = ("redirector",)
--- app/redirector/controller.py
+++ app/redirector/controller.py
@@ -1,38 +1,48 @@
 from typing import TYPE_CHECKING, Annotated
 
-from fastapi import Depends, Path, Request, status
+from fastapi import APIRouter, Depends, Path, Request, status
 from fastapi.responses import RedirectResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app import crud
 from app.api.exceptions import HTTPError
 from app.core import utils
-from app.core.config import settings
-from app.core.database import db
-from app.redirector import redirector
+from app.core.database import database
+from app.core.settings.data import Page
 
 if TYPE_CHECKING:
     from app.core.database.models import Url
 
+router: APIRouter = APIRouter(prefix="")
 
-@redirector.get("/{slug}", response_class=RedirectResponse)
+
+@router.get("/{slug}", response_class=RedirectResponse)
 async def redirect_to_url(
     request: Request,
-    slug: Annotated[str, Path()],
-    session: Annotated[AsyncSession, Depends(db.scoped_session)],
+    slug: Annotated[
+        str,
+        Path(),
+    ],
+    async_session: Annotated[
+        AsyncSession,
+        Depends(database.get_async_session),
+    ],
 ) -> RedirectResponse:
-    if slug == settings.data.NOT_FOUND_PAGE_URL:
+    if slug == Page.NOT_FOUND:
         raise HTTPError(
             errors=[],
             message="Not Found",
             status=status.HTTP_404_NOT_FOUND,
         )
 
-    url: Url | None = await crud.get_url_by_slug(session=session, slug=slug)
+    url: Url | None = await crud.get_url_by_slug(
+        async_session=async_session,
+        slug=slug,
+    )
 
-    if url is None:
+    if not url:
         return RedirectResponse(
-            url=settings.data.NOT_FOUND_PAGE_URL,
+            url=Page.NOT_FOUND.value,
             status_code=status.HTTP_307_TEMPORARY_REDIRECT,
         )
 
@@ -40,20 +50,23 @@ async def redirect_to_url(
     country: str | None = None
     if ip:
         country = await utils.get_country_by_ip(
-            session=session,
+            session=async_session,
             ip=ip,
         )
 
     await crud.create_click(
-        session=session,
+        async_session=async_session,
         url_id=url.id,
         ip=ip,
         country=country,
     )
     await crud.update_url(
-        session=session,
+        async_session=async_session,
         url=url,
         total_clicks=url.total_clicks + 1,
     )
 
-    return RedirectResponse(url=url.address, status_code=307)
+    return RedirectResponse(
+        url=url.address,
+        status_code=status.HTTP_307_TEMPORARY_REDIRECT,
+    )

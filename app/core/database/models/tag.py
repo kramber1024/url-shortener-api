687d164e1bb11c70910ccd33d895a5fa6da5768f
kramber <kramber268@gmail.com>
2024-12-01 15:37:13 +0300

    feat: Add getters and setters doc: Add docstring for __init__

--- app/core/database/models/tag.py
+++ app/core/database/models/tag.py
@@ -1,7 +1,8 @@
 from sqlalchemy import ForeignKey, String
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import Mapped, mapped_column
 
-from app.core.config import settings
+from app.core.settings.data import Name
 
 from .mixins import CreatedAtMixin, IDMixin, TableNameMixin
 from .model import Model
@@ -9,28 +10,56 @@ from .url import Url
 
 
 class Tag(Model, TableNameMixin, IDMixin, CreatedAtMixin):
-    """Model for short URL tags.
+    """Model for short ` URL ` tags."""
 
-    Attributes:
-        id (int): The unique identifier (See ` IDMixin `).
-        url_id (int): The unique identifier of the ` Url `.
-        name (str): The ` Tag ` name. Used for categorization.
-        url (Url): The ` Url ` the tag is associated with.
-
-    """
-
-    url_id: Mapped[int] = mapped_column(
-        ForeignKey(f"{Url.__tablename__}.id"),
+    _url_id: Mapped[int] = mapped_column(
+        "url_id",
+        ForeignKey(Url.id),
         nullable=False,
     )
-    name: Mapped[str] = mapped_column(
-        String(settings.data.TAG_MAX_LENGTH),
+    _name: Mapped[str] = mapped_column(
+        "name",
+        String(length=Name.MAX_LENGTH),
         nullable=False,
     )
 
     def __init__(self, *, url_id: int, name: str) -> None:
-        self.url_id = url_id
+        """Initialize a ` Tag ` model instance.
+
+        Arguments:
+            url_id (int): The unique identifier of the ` Url `
+                          ` Tag ` is associated with.
+            name (str): The ` Tag ` value. Used for categorization.
+        """
+        self._url_id = url_id
         self.name = name
 
+    @hybrid_property
+    def url_id(self) -> int:
+        """The unique identifier of the ` Url `.
+
+        Returns:
+            int: The unique identifier of the ` Url `.
+        """
+        return self._url_id
+
+    @hybrid_property
+    def name(self) -> str:
+        """The ` Tag ` value. Used for categorization.
+
+        Returns:
+            str: The ` Tag ` value.
+        """
+        return self._name
+
+    @name.inplace.setter
+    def _name_setter(self, value: str) -> None:
+        stripped_value: str = value.strip()
+
+        if not Name.MIN_LENGTH <= len(stripped_value) <= Name.MAX_LENGTH:
+            raise ValueError
+
+        self._name = stripped_value
+
     def __repr__(self) -> str:
-        return f"<Tag {self.name} for ...>"
+        return f"<{type(self).__name__} {self.name}>"

3b0d995ebe62efad891ff9ef30953aaa4031a59b
kramber <kramber268@gmail.com>
2025-01-01 21:38:57 +0300

    feat: Add module docstring

---
+++ app/core/networks/__init__.py
@@ -0,0 +1,6 @@
+"""Private module for networks.
+
+You should not use this module anywhere else in the application. It is intended
+to be used like a script, not a module. See ` app/core/networks/README.md ` for
+more information.
+"""

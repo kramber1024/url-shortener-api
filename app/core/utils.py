3558e7d0aa6907791a255c3053fd2c2d6e2418f9
kramber <kramber268@gmail.com>
2025-01-04 20:45:23 +0300

    feat: Remove unused

--- app/core/utils.py
+++ app/core/utils.py
@@ -1,14 +1,4 @@
 import datetime
-from typing import TYPE_CHECKING
-
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app import crud
-
-if TYPE_CHECKING:
-    from typing import Literal
-
-    from app.core.database.models import Network
 
 
 def now() -> int:
@@ -20,41 +10,5 @@ def now() -> int:
     Examples:
         >>> print(now())
         1723566362
-
     """
     return int(datetime.datetime.now(datetime.UTC).timestamp())
-
-
-async def get_country_by_ip(*, session: AsyncSession, ip: str) -> str | None:
-    """Get the country code of the given ` ip `.
-
-    Country code is a two letter code as per \
-        [**ISO 3166-1 alpha-2**](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2).
-
-    Args:
-        session (AsyncSession): The database session.
-        ip (str): The IP address to check.
-
-    Returns:
-        Country code if found, otherwise ` None `.
-    """
-    octets: tuple[Literal[3], Literal[2], Literal[1]] = (3, 2, 1)
-    checked_networks: set[str] = set()
-
-    for octet in octets:
-        networks: list[Network] = await crud.get_networks_by_ip(
-            session=session,
-            ip=ip,
-            octets=octet,
-        )
-
-        for network in networks:
-            if network.address in checked_networks:
-                continue
-
-            if ip in network:
-                return network.country
-
-            checked_networks.add(str(network))
-
-    return None

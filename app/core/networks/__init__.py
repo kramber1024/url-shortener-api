fed43ce1f462450e1ce50a90fa0e6fc26bc01c77
kramber <kramber268@gmail.com>
2025-01-09 15:19:09 +0300

    refactor: Move parsing module out of app

--- app/core/networks/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Private module for networks.
-
-You should not use this module anywhere else in the application. It is intended
-to be used like a script, not a module. See ` app/core/networks/README.md ` for
-more information.
-"""

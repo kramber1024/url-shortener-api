5b640ac540d1ae4a7d9888cae3ba2e569fb015fa
kramber <kramber268@gmail.com>
2025-01-10 14:49:11 +0300

    feat: Update ` __all__ ` doc: Add module level doc

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,26 +1,22 @@
+"""CRUD operations.
+
+This module provides functions to interact with the database.
+"""
+
 from .click import create_click
-from .network import create_network, get_networks_by_ip
 from .status import create_status
 from .tag import create_tag
-from .url import (
-    create_url,
-    get_url_by_slug,
-    get_urls_by_page_and_limit,
-    update_url,
-)
+from .url import create_url, get_url_by_slug, update_url
 from .user import create_user, get_user_by_email, get_user_by_id
 
-__all__ = (
+__all__ = [
     "create_click",
-    "create_network",
     "create_status",
     "create_tag",
     "create_url",
     "create_user",
-    "get_networks_by_ip",
     "get_url_by_slug",
-    "get_urls_by_page_and_limit",
     "get_user_by_email",
     "get_user_by_id",
     "update_url",
-)
+]

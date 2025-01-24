991f8cc4c3d67e282f76cbd8024cbccaf6fc6a4a
kramber <kramber268@gmail.com>
2025-01-24 22:34:47 +0300

    refactor: Update mixin

--- app/core/database/models/mixins/tablename.py
+++ app/core/database/models/mixins/tablename.py
@@ -2,7 +2,11 @@ from sqlalchemy.ext.declarative import declared_attr
 
 
 class TableNameMixin:
-    """A mixin for setting the table name in the database."""
+    """Mixin for setting the name of a table in a database.
+
+    Automatically sets the name of a table to the name of a class in the plural
+    form.
+    """
 
     @declared_attr.directive
     def __tablename__(self) -> str:
@@ -10,16 +14,13 @@ class TableNameMixin:
 
         Raises:
             AttributeError: If the class does not have a __name__ attribute.
-
-        Returns:
-            str: The name of the table in the database.
         """
         if not hasattr(self, "__name__") or not isinstance(self.__name__, str):
             raise AttributeError
 
         table_name: str = self.__name__
 
-        if table_name[-1] == "s":
+        if table_name.endswith("s"):
             return table_name + "es"
 
         return table_name + "s"

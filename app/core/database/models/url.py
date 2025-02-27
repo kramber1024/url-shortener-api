087519dec7e39382ba0c6492f67fa450113c1f64
kramber <kramber268@gmail.com>
2025-02-27 10:20:23 +0300

    feat: Add setters and getters doc: Update documentation

--- app/core/database/models/url.py
+++ app/core/database/models/url.py
@@ -1,11 +1,12 @@
 from typing import TYPE_CHECKING
 
 from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from app.core.config import settings
+from app.core.settings.data import Slug, Source
 
-from .mixins import CreatedAtMixin, IDMixin, TableNameMixin, UpdatedAtMixin
+from .mixins import CreatedAtMixin, IdMixin, TableNameMixin, UpdatedAtMixin
 from .model import Model
 from .user import User
 
@@ -14,36 +15,28 @@ if TYPE_CHECKING:
     from .tag import Tag
 
 
-class Url(Model, TableNameMixin, IDMixin, UpdatedAtMixin, CreatedAtMixin):
-    """Model for short URLs.
+class Url(Model, TableNameMixin, IdMixin, UpdatedAtMixin, CreatedAtMixin):
+    """Model for shortened URL."""
 
-    Attributes:
-        id (int): The unique identifier (See ` IDMixin `).
-        user_id (int): The unique identifier of the ` User ` aka author.
-        slug (str): The short URL address.
-        address (str): The long URL address.
-        total_clicks (int): The total number of uses of the short URL.
-        author (User): The author of the short URL.
-        clicks (list[Click]): The list of ` Click`'s on the short URL.
-
-    """
-
-    user_id: Mapped[int] = mapped_column(
+    _user_id: Mapped[int] = mapped_column(
         "user_id",
-        ForeignKey(f"{User.__tablename__}.id"),
+        ForeignKey(User.id),
+        nullable=False,
         primary_key=True,
+    )
+    _source: Mapped[str] = mapped_column(
+        "source",
+        String(length=Source.MAX_LENGTH),
         nullable=False,
     )
-    slug: Mapped[str] = mapped_column(
-        String(settings.data.SHORT_URL_MAX_LENGTH),
+    _slug: Mapped[str] = mapped_column(
+        "slug",
+        String(length=Slug.MAX_LENGTH),
         nullable=False,
         unique=True,
     )
-    address: Mapped[str] = mapped_column(
-        String(settings.data.URL_MAX_LENGTH),
-        nullable=False,
-    )
-    total_clicks: Mapped[int] = mapped_column(
+    _total_clicks: Mapped[int] = mapped_column(
+        "total_clicks",
         Integer(),
         nullable=False,
     )
@@ -67,13 +60,61 @@ class Url(Model, TableNameMixin, IDMixin, UpdatedAtMixin, CreatedAtMixin):
         self,
         *,
         user_id: int,
+        source: str,
         slug: str,
-        address: str,
     ) -> None:
-        self.user_id = user_id
+        """Initialize a ` Url ` model instance.
+
+        Args:
+            user_id: The unique identifier of the ` User `.
+            source: The original URL address.
+            slug: The unique slug that identifies the shortened URL.
+        """
+        self._user_id = user_id
+        self.source = source
         self.slug = slug
-        self.address = address
         self.total_clicks = 0
 
+    @hybrid_property
+    def user_id(self) -> int:
+        """The unique identifier of the ` User `."""
+        return self._user_id
+
+    @hybrid_property
+    def source(self) -> str:
+        """The original URL address."""
+        return self._source
+
+    @source.inplace.setter
+    def _source_setter(self, value: str) -> None:
+        if not Source.validate_length(value):
+            raise ValueError
+
+        self._source = value
+
+    @hybrid_property
+    def slug(self) -> str:
+        """The unique slug that identifies the shortened URL."""
+        return self._slug
+
+    @slug.inplace.setter
+    def _slug_setter(self, value: str) -> None:
+        if not Slug.validate_length(value):
+            raise ValueError
+
+        self._slug = value
+
+    @hybrid_property
+    def total_clicks(self) -> int:
+        """The total number of ` Click `'s."""
+        return self._total_clicks
+
+    @total_clicks.inplace.setter
+    def _total_clicks_setter(self, value: int) -> None:
+        if value < 0:
+            raise ValueError
+
+        self._total_clicks = value
+
     def __repr__(self) -> str:
-        return f"<{type(self).__name__} /{self.slug} -> {self.address}>"
+        return f"<{type(self).__name__} /{self.slug} -> {self.source}>"

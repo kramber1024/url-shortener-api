c442a410a2e45fae7a7d12e9ff2c449f1ceaa9c0
kramber <kramber268@gmail.com>
2025-03-13 10:36:04 +0300

    test(database): Add 100% tests for Tag model
    Signed-off-by: kramber <kramber268@gmail.com>

--- tests/core/database/models/test_tag.py
+++ tests/core/database/models/test_tag.py
@@ -11,57 +11,75 @@ def url_id() -> int:
 
 @pytest.fixture(scope="module")
 def name() -> str:
-    return "tag"
-
-
-@pytest.fixture(scope="module")
-def tag(url_id: int, name: str) -> Tag:
-    return Tag(url_id=url_id, name=name)
-
-
-@pytest.mark.parametrize(
-    "name",
-    [
-        "a" * (Name.MIN_LENGTH - 1),
-        "a" * (Name.MAX_LENGTH + 1),
-    ],
-)
-def test_tag_init_invalid_name(url_id: int, name: str) -> None:
-    with pytest.raises(ValueError, match=r"^$"):
-        Tag(url_id=url_id, name=name)
+    return "name"
 
 
 def test_tag_init(url_id: int, name: str) -> None:
     tag: Tag = Tag(url_id=url_id, name=name)
 
-    assert hasattr(tag, "id")
     assert tag.url_id == url_id
     assert tag.name == name
-    assert hasattr(tag, "created_at")
 
 
-def test_tag_url_id_property(url_id: int, name: str) -> None:
+@pytest.mark.parametrize("url_id", [123321, 77850370187, 14991194284, -1, 0])
+def test_tag_property_url_id(url_id: int, name: str) -> None:
     tag: Tag = Tag(url_id=url_id, name=name)
 
     assert tag.url_id == url_id
 
 
-def test_tag_name_property(tag: Tag, name: str) -> None:
+@pytest.mark.parametrize(
+    "name",
+    [
+        "t" * Name.MIN_LENGTH,
+        "t" * Name.MAX_LENGTH,
+        "name",
+        "tag-name",
+        "tag_name",
+    ],
+)
+def test_tag_property_name(url_id: int, name: str) -> None:
+    tag: Tag = Tag(url_id=url_id, name=name)
+
     assert tag.name == name
 
 
-def test_tag_name_property_setter_invalid(tag: Tag) -> None:
-    with pytest.raises(ValueError, match=r"^$"):
-        tag.name = ""
+@pytest.mark.parametrize(
+    "name",
+    [
+        "t" * (Name.MIN_LENGTH - 1),
+        "t" * (Name.MAX_LENGTH + 1),
+        "t" * (Name.MAX_LENGTH + 100),
+        "t" * (Name.MAX_LENGTH + 1000),
+        "t" * (Name.MAX_LENGTH + 10000),
+    ],
+)
+def test_tag_property_name_invalid_length(url_id: int, name: str) -> None:
+    tag: Tag = Tag(url_id=url_id, name="t" * Name.MIN_LENGTH)
+
+    with pytest.raises(ValueError, match="^$"):
+        tag.name = name
 
 
-def test_tag_name_property_setter(tag: Tag) -> None:
-    new_name: str = "name"
+@pytest.mark.parametrize(
+    "name",
+    [
+        "t" * Name.MIN_LENGTH,
+        "t" * Name.MAX_LENGTH,
+        "name",
+        "tag-name",
+        "tag_name",
+    ],
+)
+def test_tag_property_name_valid_length(url_id: int, name: str) -> None:
+    tag: Tag = Tag(url_id=url_id, name="t" * Name.MIN_LENGTH)
 
-    tag.name = new_name
+    tag.name = name
 
-    assert tag.name == new_name
+    assert tag.name == name
 
 
-def test_tag_repr(tag: Tag) -> None:
-    assert repr(tag) == f"<Tag {tag.name}>"
+def test_tag_repr(url_id: int, name: str) -> None:
+    tag: Tag = Tag(url_id=url_id, name=name)
+
+    assert repr(tag) == f"<Tag {name}>"

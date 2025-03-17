d7755209e03e454a1b6791667ddc908ee4010ca8
kramber <kramber268@gmail.com>
2025-03-17 15:05:48 +0300

    doc: Update docstring feat: Minor refactoring
    Signed-off-by: kramber <kramber268@gmail.com>

--- app/core/database/database.py
+++ app/core/database/database.py
@@ -1,10 +1,8 @@
-from asyncio import current_task
 from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
     AsyncSession,
-    async_scoped_session,
     async_sessionmaker,
     create_async_engine,
 )
@@ -15,33 +13,32 @@ from .models import Model
 
 
 class Database:
-    _async_engine: AsyncEngine
-    _async_sessionmaker: async_sessionmaker[AsyncSession]
-    _url: str
+    """Database manager.
+
+    This class is responsible for managing the database interactions.
+    """
 
     def __init__(self, url: str) -> None:
-        self._async_engine = create_async_engine(
+        self._async_engine: AsyncEngine = create_async_engine(
             url=url,
             echo=False,
         )
-        self._async_sessionmaker = async_sessionmaker(
-            bind=self._async_engine,
-            autocommit=False,
-            autoflush=False,
-            expire_on_commit=False,
-        )
-        self._url = url.split("///")[1]
-
-    async def get_async_session(
-        self,
-    ) -> AsyncGenerator[async_scoped_session[AsyncSession], None]:
-        try:
-            async_session: async_scoped_session[AsyncSession] = (
-                async_scoped_session(
-                    session_factory=self._async_sessionmaker,
-                    scopefunc=current_task,
-                )
+        self._async_sessionmaker: async_sessionmaker[AsyncSession] = (
+            async_sessionmaker(
+                bind=self._async_engine,
+                autoflush=False,
+                expire_on_commit=False,
+                autocommit=False,
             )
+        )
+
+    async def get_async_session(self) -> AsyncGenerator[AsyncSession]:
+        """Get an async session from the sessionmaker.
+
+        This is a ` FastAPI ` dependency.
+        """
+        async_session: AsyncSession = self._async_sessionmaker()
+        try:
             yield async_session
         finally:
             await async_session.close()
@@ -49,18 +46,18 @@ class Database:
     async def shutdown(self) -> None:
         """Shutdown the database connections.
 
-        This method ` must ` be called when the application is shutting down.
+        This method must be called when the application is shutting down.
         """
         await self._async_engine.dispose(close=True)
 
     async def create_tables(self, *, hard_reset: bool = False) -> None:
-        async with self._async_engine.begin() as connection:
+        async with self._async_engine.begin() as async_connection:
             if hard_reset:
-                await connection.run_sync(Model.metadata.drop_all)
+                await async_connection.run_sync(Model.metadata.drop_all)
 
-            await connection.run_sync(Model.metadata.create_all)
+            await async_connection.run_sync(Model.metadata.create_all)
 
 
 database: Database = Database(
-    url=settings.database.DATABASE_URL,
+    url=settings.database.URL,
 )

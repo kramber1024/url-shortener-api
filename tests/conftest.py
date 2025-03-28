cfc6e208dca1519e1e354087c5d6a97c128a7f78
kramber <kramber268@gmail.com>
2025-03-28 18:54:56 +0300

    test: Add fixtures for database interactions
    Signed-off-by: kramber <kramber268@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,10 @@
-# from collections.abc import AsyncGenerator
+from collections.abc import AsyncGenerator
 
 import pytest
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import AsyncSession
 
-# import pytest_asyncio
-# # from httpx import ASGITransport, AsyncClient
-# from sqlalchemy.ext.asyncio import AsyncSession
-# # from app.__main__ import app
-# from app.core.database import Database
-# # from app.core.database import database as app_database
+from app.core.database import Database
 from app.core.settings import settings
 
 
@@ -19,39 +16,21 @@ def setup() -> None:
     )
 
 
-# @pytest_asyncio.fixture(scope="session")
-# async def database() -> AsyncGenerator[Database, None]:
-#     database: Database = Database(url=settings.database.URL)
-#     await database.create_tables(hard_reset=True)
-#     yield database
-#     await database.shutdown()
+@pytest_asyncio.fixture()
+async def database() -> AsyncGenerator[Database, None]:
+    database: Database = Database(url=settings.database.URL)
+
+    await database.create_tables(hard_reset=True)
+    yield database
+    await database.shutdown()
 
 
-# @pytest_asyncio.fixture()
-# async def async_session(
-#     database: Database,
-# ) -> AsyncGenerator[AsyncSession, None]:
-#     async_session: AsyncSession = database._async_sessionmaker()
-#     try:
-#         yield async_session
-#     finally:
-#         await async_session.close()
-
-
-# @pytest_asyncio.fixture(scope="session")
-# async def async_client(database: Database) -> AsyncGenerator[AsyncClient, None]:
-#     app.dependency_overrides[app_database.get_async_session] = (
-#         database.get_async_session
-#     )
-
-#     async with AsyncClient(
-#         transport=ASGITransport(app),
-#         base_url=(
-#             f"http://{settings.development.HOST}:{settings.development.PORT}"
-#         ),
-#         headers={"Content-Type": "application/json"},
-#         timeout=10,
-#     ) as async_client:
-#         yield async_client
-
-#     app.dependency_overrides.clear()
+@pytest_asyncio.fixture()
+async def async_session(
+    database: Database,
+) -> AsyncGenerator[AsyncSession, None]:
+    async_session: AsyncSession = database._async_sessionmaker()
+    try:
+        yield async_session
+    finally:
+        await async_session.close()
